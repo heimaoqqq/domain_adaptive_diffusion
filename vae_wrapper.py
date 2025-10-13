c37a697b0bde04ce936e3c5350e394318e57e741
heimaoqqq <428315424@qq.com>
2025-10-13 09:56:13 +0800

    Update vae_wrapper.py

--- vae_wrapper.py
+++ vae_wrapper.py
@@ -43,7 +43,6 @@ class VAEInterface:
                 block_out_channels=[128, 256, 512, 512],
                 layers_per_block=2,
                 act_fn="silu",
-                latent_channels=4,
                 norm_num_groups=32,
                 sample_size=256
             )
