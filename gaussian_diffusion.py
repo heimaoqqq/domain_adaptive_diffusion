d586b17a2e88f702060f444d448b20a1a96c1df0
heimaoqqq <428315424@qq.com>
2025-10-13 10:00:12 +0800

    Update gaussian_diffusion.py

--- gaussian_diffusion.py
+++ gaussian_diffusion.py
@@ -11,8 +11,12 @@ import math
 import numpy as np
 import torch as th
 
-from .nn import mean_flat
-from .losses import normal_kl, discretized_gaussian_log_likelihood
+try:
+    from .nn import mean_flat
+    from .losses import normal_kl, discretized_gaussian_log_likelihood
+except ImportError:
+    from nn import mean_flat
+    from losses import normal_kl, discretized_gaussian_log_likelihood
 
 
 def get_named_beta_schedule(schedule_name, num_diffusion_timesteps):
